55837fea7c2d8bb848fd2033fabcbc207a672a9e
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-12 11:33:35 -0300

    create function to read from file

--- C/day1/main.c
+++ C/day1/main.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void readFile(char * path, char * text) {
+	FILE *fp = fopen(path, "r");
+	char buf[2560];
+
+	if (fp != NULL) {
+		while(fgets(buf, 2560, fp)) {
+			strcat(text, buf);
+		}
+	} else {
+		printf("Could not open file");
+	}
+	
+	fclose(fp);
+}
+
 int main() {
+	char text[2560];
+
+	readFile("./test", text);
+	printf("%s", text);
 	return 0;
 }
